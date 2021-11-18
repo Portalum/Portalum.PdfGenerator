65c5621baa588a2fe891f2fcc3d756e3ee8a4049
EnjoX <enjoxrox@gmail.com>
2021-11-18 13:08:30 +0100

    init

---
+++ src/controllers/pdfController.ts
@@ -0,0 +1,27 @@
+import { TDocumentDefinitions } from "pdfmake/interfaces";
+import {
+    Body,
+    Controller,
+    Get,
+    Path,
+    Post,
+    Query,
+    Route,
+    SuccessResponse,
+  } from "tsoa";
+import { PdfCreateResponseDto } from "../models/pdfCreateResponseDto";
+import { PdfService } from "../services/pdfService";
+
+
+  @Route("pdf")
+  export class PdfController extends Controller {
+
+    @Post()
+    public async getPdf(@Body() request: any) : Promise<PdfCreateResponseDto>
+    {
+        const pdfService = new PdfService();
+        const result = await pdfService.createPdf(request);
+        return { pdf: result };
+    }
+
+  }
